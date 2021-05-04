f599a4beb41ea23a657dc29da3037c7eb94bb35c
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-04 15:17:48 +0530

    Added: Code for list

---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doListDemo();
+        }
+        private static void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDemo");
+            //Creating List
+            List<string> list = new List<string>();
+            //Adding object to the list
+            list.Add("Ravi");
+            list.Add("Vijay");
+            list.Add("Ravi");
+            list.Add("Ajay");
+
+            //Iterating list elements
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
